af95b5cd3807b7eb6f6db3bbc7603ccc271d5673
Christian Couder <chriscool@tuxfamily.org>
2014-11-03 00:22:56 +0100

    Add an optional seed parameter
    We have to use math/rand instead of crypto/rand if seed
    is provided.
    
    License: MIT
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- random/random.go
+++ random/random.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"crypto/rand"
+	randcrypto "crypto/rand"
+	randmath "math/rand"
 	"fmt"
 	"io"
 	"os"
 	"strconv"
+	"bytes"
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	l := len(os.Args)
+	if l != 2 && l != 3 {
 		usageError()
 	}
 
@@ -18,14 +21,25 @@ func main() {
 		usageError()
 	}
 
-	if err := writeRandomBytes(num, os.Stdout); err != nil {
+	if l == 2 {
+		err = writeRandomBytes(num, os.Stdout)
+	} else {
+		seed, err2 := strconv.Atoi(os.Args[2])
+		if err2 != nil {
+			usageError()
+		}
+		err = writePseudoRandomBytes(num, os.Stdout, seed)
+	}
+
+	if err != nil {
 		die(err)
 	}
 }
 
 func usageError() {
-	fmt.Fprintf(os.Stderr, "Usage: %s <int>\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "Output <int> random bytes (from Go's crypto/rand)\n")
+	fmt.Fprintf(os.Stderr, "Usage: %s <count> [<seed>]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "If <seed> is given, output <count> pseudo random bytes made from <seed> (from Go's math/rand)\n")
+	fmt.Fprintf(os.Stderr, "Otherwise, output <count> random bytes (from Go's crypto/rand)\n")
 	os.Exit(-1)
 }
 
@@ -35,7 +49,19 @@ func die(err error) {
 }
 
 func writeRandomBytes(num int, w io.Writer) error {
-	r := &io.LimitedReader{R: rand.Reader, N: int64(num)}
+	r := &io.LimitedReader{R: randcrypto.Reader, N: int64(num)}
 	_, err := io.Copy(w, r)
 	return err
 }
+
+func writePseudoRandomBytes(num int, w io.Writer, seed int) error {
+	randmath.Seed(int64(seed))
+	b := make([]byte, num)
+	for i := 0 ; i < num ; i++ {
+		b[i] = byte(randmath.Intn(256))
+	}
+	r := bytes.NewReader(b)
+	_, err := io.Copy(w, r)
+	return err
+}
+
